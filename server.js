7633b99c61edd3dac52bff01a672712d705684bf
junaid <junaidkhan1@gmx.com>
2019-02-02 23:35:55 +0000

    hbs express added

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 //const bodyParser = require('body-parser');
+
+const exphbs  = require('express-handlebars');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const path = require('path');
 const config = require('config');
 const helmet = require('helmet')
 const logger = require('./logger');
@@ -9,9 +14,13 @@ const express = require('express');
 const app = new express();
 const port = process.env.PORT || 8000;
 const genres  = require('./genres');
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views',path.join(__dirname,'views') );
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.use(helmet());
 
 app.use(logger);
@@ -19,14 +28,16 @@ app.use(authentication());
 
 if(app.get('env') === 'development'){
 app.use(morgan('tiny'));
+startupDebugger('morgan enable')
 }
 
 //configuration
 
-console.log(`Application Name : ${config.get('name')}`);
-console.log(`email server : ${config.get('mail.host')}`);
+startupDebugger(`Application Name : ${config.get('name')}`);
+startupDebugger(`email server : ${config.get('mail.host')}`);
 
-
+//db
+dbDebugger('connected to the database');
 // console.log(`NODE_ENV : ${process.env.NODE_ENV}`);
 // console.log(`app: ${app.get('env')}`)
 const genreList = genres.genres; 
@@ -34,7 +45,7 @@ const genreList = genres.genres;
 
 app.get('/', (req, res)=>{
 
-    res.send('Welcome to Vidly API main Page!!!!');
+    res.render('index.hbs',{title:"REST API APP"})
 
 })
 
